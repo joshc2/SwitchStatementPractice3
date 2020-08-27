1d947ec2678c0825568383cb0cd48e5ab49be6df
Joshua Cabrera <cabrerajoshua25@gmail.com>
2020-08-26 23:40:25 -0600

    Add project files.

---
+++ SwitchStatementPractice3/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>	
+
+using namespace std;
+
+
+bool is_paren(char t) {
+
+	switch (t) {
+	case '(': return true;
+	case '[':return true;
+	case '{': return true;
+	default: return false;
+	}
+
+}
+
+int main() {
+
+	/*char myChar;
+	cin >> myChar;
+
+	
+	if (is_paren(myChar)) { cout << "true"; }
+	else { cout << "false"; }
+*/
+
+	if (1 == true) {
+		cout << "True!";
+	}
+	else {
+		cout << "False!";
+	}
+	return 0;
+}
